3dce996474ce019698e14ef8272ae425a8abf4e3
Clóvis Chakrian <clovischakrian@pop-os.localdomain>
2022-12-15 17:12:54 -0300

    updated the salt rounds env variable name

--- src/controllers/ManagerController.ts
+++ src/controllers/ManagerController.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import prismaClient from "../database/prismaClient";
 import * as bcrypt from 'bcrypt';
 import jwt from 'jwt-simple';
+import { managerSchema } from '../yupSchemas/schemas';
+import { ValidationError } from "yup";
+import { IManager } from '../@types/interfaces';
 
 export default {
   async create(req: Request, res: Response) {
@@ -14,15 +17,41 @@ export default {
       password
     } = req.body;
 
-    bcrypt.hash(password, Number(process.env.SALTROUNDS)).then(hash => {
-      const manager = {
-        name,
-        lastname,
-        email,
-        password: hash
-      };
+    const data = {
+      name,
+      lastname,
+      email,
+      password
+    };
 
-      return res.json(manager);
+    managerSchema.validate(data, {
+      abortEarly: false
+    }).then(() => {
+      bcrypt.hash(password, Number(process.env.SALT_ROUNDS)).then(async (hash) => {
+        const manager = {
+          name,
+          lastname,
+          email,
+          password: hash
+        };
+
+        await prismaClient.manager.create({
+          data: manager
+        }).then((resp: IManager) => {
+          const token = jwt.encode(resp.id, `${process.env.JWT_SECRET}`)
+          return res.status(201).json({token: token});
+        }).catch((err) => {
+          return res.status(500).json({
+            message: 'Houve um erro interno do servidor.',
+            error: err
+          });
+        })
+      });
+    }).catch((err: ValidationError) => {
+      return res.status(400).json({
+        message: 'Houve um erro na requisição.',
+        errors: err.errors
+      });
     });
   }
 };
