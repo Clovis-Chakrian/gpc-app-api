b722a267605e3ed7d3c140ede3fcfca4a7f8875f
Clóvis Chakrian <clovischakrian@pop-os.localdomain>
2022-12-15 17:29:07 -0300

    added parents routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import managerRoutes from "./managerRoutes";
+import parentsRoutes from "./parentsRoutes";
 
-const routes = [managerRoutes];
+const routes = [managerRoutes, parentsRoutes];
 
 export default routes;
